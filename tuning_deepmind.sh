34f81859afffcc7ed62dbdee65ea3b75965a1b93
Piji Li <pagelee.sd@gmail.com>
2018-10-24 17:22:18 +0800

    fix path

--- tuning_deepmind.sh
+++ tuning_deepmind.sh
@@ -5,6 +5,6 @@ for f in $FILES; do
     python -u main.py ${f##*/}
     python prepare_rouge.py
     cd ./deepmind/result/ 
-    perl /home/pijili/tools/ROUGE-1.5.5/ROUGE-1.5.5.pl -n 4 -w 1.2 -m -2 4 -u -c 95 -r 1000 -f A -p 0.5 -t 0  myROUGE_Config.xml C
+    perl Home-of-ROUGE-1.5.5/ROUGE-1.5.5.pl -n 4 -w 1.2 -m -2 4 -u -c 95 -r 1000 -f A -p 0.5 -t 0  myROUGE_Config.xml C
     cd ../../
 done
